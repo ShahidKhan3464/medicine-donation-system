9bcaf74fe5695486c797247a9da16f58c33d1d09
shahidKhan <shahidafridi26360@gmail.com>
2023-01-28 01:19:02 -0800

    remove unrequired dependencies

--- client/src/modules/admin/pages/users.js
+++ client/src/modules/admin/pages/users.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import { PrimaryHeading, TableContainer } from '../../../globalStyle';
 import sweetAlert from "../../../components/sweetAlert";
 import { FaTrash, FaEdit } from 'react-icons/fa';
